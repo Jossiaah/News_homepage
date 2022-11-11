dde3fc3053d3962c0f1651a13ff6770906c33890
Josiah <jcast1998@gmail.com>
2022-11-10 20:24:36 -0800

    Commit1

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./Components/Header";
+
+const App = () => {
+  return <div>
+          <Header />
+        </div>;
+};
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>This the Header Hoe!</div>
+  )
+}
+
+export default Header
