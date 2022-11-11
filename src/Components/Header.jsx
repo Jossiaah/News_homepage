6338ff37a207825b72619a67cea691ca5a7c2bbd
Josiah <jcast1998@gmail.com>
2022-11-10 21:48:20 -0800

    Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
+import MyImage from '/Users/josiahcastillo/Desktop/small_projects/news_homepage_reactjs/src/assets/images/logo.svg'
+
+
+const Container = styled.div`
+    padding: 80px;
+    margin-top: 2rem;
+`;
+const Nav = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+const Logo = styled.img`
+    height: 50px;
+
+`;
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+`;
+const NavLinks = styled.div`
+    margin-left: 25px;
+`;
 
 const Header = () => {
   return (
-    <div>This the Header Hoe!</div>
+    <Container>
+        <Nav>
+            <Logo src={MyImage} alt='' />
+            <Wrapper>
+                <NavLinks>Home</NavLinks>
+                <NavLinks>Home</NavLinks>
+                <NavLinks>Home</NavLinks>
+                <NavLinks>Home</NavLinks>
+                <NavLinks>Home</NavLinks>
+            </Wrapper>
+        </Nav>
+    </Container>
   )
 }
 
